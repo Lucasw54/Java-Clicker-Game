81db0780328afe18f69c15092e2044ae3f4d134e
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2022-03-08 22:59:33 -0500

    Clicker_Game.java
    Added GUI modes

--- Clicker Game/com.LucasWall.Clicker_Game/Clicker_Game.java
+++ Clicker Game/com.LucasWall.Clicker_Game/Clicker_Game.java
@@ -41,10 +41,20 @@ public class Clicker_Game implements ActionListener
 	private static JButton Quit1;
 	private static JButton Save;
 	private static JButton Load;
+	private static JButton Back;
 	private static JButton Options;
 	public static JProgressBar bar = new JProgressBar();
 	public static int counter = 0;
 	public static boolean load = false;
+	public static JLabel GUImodeLabel;
+	public static JButton GUImode1Label;
+	public static JButton GUImode2Label;
+	public static JButton GUImode3Label;
+	public static JButton GUImode4Label;
+	public static JButton GUImode5Label;
+	public static JButton GUImode6Label;
+	public static JButton GUImode7Label;
+	
 	
 	
 	//------Game Objects
@@ -64,10 +74,17 @@ public class Clicker_Game implements ActionListener
 	public enum Actions
 	{//Beginning of Actions
 		LOAD,
-		BRACKETS,
+		SAVE,
 		RETURN,
 		QUIT,
-		RECALCULATE
+		OPTIONS,
+		GUIMODE1,
+		GUIMODE2,
+		GUIMODE3,
+		GUIMODE4,
+		GUIMODE5,
+		GUIMODE6,
+		GUIMODE7
 	}//End of Actions
 	
 	public static void Load() {
@@ -163,7 +180,7 @@ public class Clicker_Game implements ActionListener
 		
 		Save = new JButton("Save");
 		Save.setBounds(560, 10, 80, 25);
-		Save.setActionCommand(Actions.QUIT.name());
+		Save.setActionCommand(Actions.SAVE.name());
 		Save.addActionListener(instance);
 		panel.add(Save);
 		Save.setVisible(false);
@@ -177,10 +194,18 @@ public class Clicker_Game implements ActionListener
 		
 		Options = new JButton("Options");
 		Options.setBounds(1190, 10, 80, 25);
-		Options.setActionCommand(Actions.QUIT.name());
+		Options.setActionCommand(Actions.OPTIONS.name());
 		Options.addActionListener(instance);
 		panel.add(Options);
 		Options.setVisible(false);
+		
+		Back = new JButton("Back");
+		Back.setBounds(90, 10, 80, 25);
+		Back.setActionCommand(Actions.RETURN.name());
+		Back.addActionListener(instance);
+		panel.add(Back);
+		Back.setVisible(false);
+		
 		
 		PencilLabel = new JLabel("Pencils: " + number.format(Pencils));
 		PencilLabel.setBounds(20, 70, 1240, 25);
@@ -188,7 +213,68 @@ public class Clicker_Game implements ActionListener
 		PencilLabel.setFont(new Font("Serif", Font.PLAIN, 24));
 		PencilLabel.setVisible(false);
 		
+		//-------Options Window
+		GUImodeLabel = new JLabel("Window Size: ");
+		GUImodeLabel.setBounds(30,100,200,20);
+		panel.add(GUImodeLabel);
+		GUImodeLabel.setVisible(false);
+		GUImodeLabel.setFont(new Font("Serif", Font.PLAIN, 24));
 		
+		GUImode1Label = new JButton("Default");
+		GUImode1Label.setBounds(30,280,200,40);
+		GUImode1Label.setActionCommand(Actions.GUIMODE1.name());
+		GUImode1Label.addActionListener(instance);
+		panel.add(GUImode1Label);
+		GUImode1Label.setVisible(false);
+		GUImode1Label.setFont(new Font("Serif", Font.PLAIN, 24));
+		
+		GUImode2Label = new JButton("2048 x 1280");
+		GUImode2Label.setBounds(30,130,200,40);
+		GUImode2Label.setActionCommand(Actions.GUIMODE2.name());
+		GUImode2Label.addActionListener(instance);
+		panel.add(GUImode2Label);
+		GUImode2Label.setVisible(false);
+		GUImode2Label.setFont(new Font("Serif", Font.PLAIN, 24));
+		
+		GUImode3Label = new JButton("1650 x 1050");
+		GUImode3Label.setBounds(30,180,200,40);
+		GUImode3Label.setActionCommand(Actions.GUIMODE3.name());
+		GUImode3Label.addActionListener(instance);
+		panel.add(GUImode3Label);
+		GUImode3Label.setVisible(false);
+		GUImode3Label.setFont(new Font("Serif", Font.PLAIN, 24));
+		
+		GUImode4Label = new JButton("1440 x 900");
+		GUImode4Label.setBounds(30,230,200,40);
+		GUImode4Label.setActionCommand(Actions.GUIMODE4.name());
+		GUImode4Label.addActionListener(instance);
+		panel.add(GUImode4Label);
+		GUImode4Label.setVisible(false);
+		GUImode4Label.setFont(new Font("Serif", Font.PLAIN, 24));
+
+		GUImode5Label = new JButton("1152 x 720");
+		GUImode5Label.setBounds(30,330,200,40);
+		GUImode5Label.setActionCommand(Actions.GUIMODE5.name());
+		GUImode5Label.addActionListener(instance);
+		panel.add(GUImode5Label);
+		GUImode5Label.setVisible(false);
+		GUImode5Label.setFont(new Font("Serif", Font.PLAIN, 24));
+		
+		GUImode6Label = new JButton("1024 x 768");
+		GUImode6Label.setBounds(30, 380,200,40);
+		GUImode6Label.setActionCommand(Actions.GUIMODE6.name());
+		GUImode6Label.addActionListener(instance);
+		panel.add(GUImode6Label);
+		GUImode6Label.setVisible(false);
+		GUImode6Label.setFont(new Font("Serif", Font.PLAIN, 24));
+		
+		GUImode7Label = new JButton("800 x 600");
+		GUImode7Label.setBounds(30,430,200,40);
+		GUImode7Label.setActionCommand(Actions.GUIMODE7.name());
+		GUImode7Label.addActionListener(instance);
+		panel.add(GUImode7Label);
+		GUImode7Label.setVisible(false);
+		GUImode7Label.setFont(new Font("Serif", Font.PLAIN, 24));
 		
 		//set frame to visible
 		frame.setVisible(true);
@@ -238,6 +324,7 @@ public class Clicker_Game implements ActionListener
 			}//End of catch
 		}
 	}
+	
 	public static void fill()
 	{//Beginning of fill
 		int counter = 0;
@@ -248,7 +335,6 @@ public class Clicker_Game implements ActionListener
 			try {//Beginning of try
 				//Make invisble while loading
 				Thread.sleep(25);
-				Load.setVisible(true);
 				Load.setBounds(850, 610, 80, 25);
 				}//End of try
 			
@@ -259,8 +345,27 @@ public class Clicker_Game implements ActionListener
 			
 			counter +=1;
 		}//End of while
+		Load.setVisible(true);
 	}//End of fill
+	
+//--------------Options Pane
+	public static void options() {
+		Save.setVisible(false);
+		Load.setVisible(false);
+		Options.setVisible(false);
+		Back.setVisible(true);
+		
+		PencilLabel.setVisible(false);
+		GUImodeLabel.setVisible(true);
+		GUImode1Label.setVisible(true);
+		GUImode2Label.setVisible(true);
+		GUImode3Label.setVisible(true);
+		GUImode4Label.setVisible(true);
+		GUImode5Label.setVisible(true);
+		GUImode6Label.setVisible(true);
+		GUImode7Label.setVisible(true);
 
+	}
 	//If button pressed
 	@Override
 	public void actionPerformed(ActionEvent e)
@@ -274,15 +379,29 @@ public class Clicker_Game implements ActionListener
 		}//End of if
 		
 		//Button Tax Brackets
-		else if (e.getActionCommand() == Actions.BRACKETS.name())
+		else if (e.getActionCommand() == Actions.SAVE.name())
 		{//Beginning of else if
-			frame.dispose();
+			
 		}//End of else if
 		
 		//Return
 		else if (e.getActionCommand() == Actions.RETURN.name())
 		{//Beginning of else if
-			frame.dispose();
+			Game();
+			Save.setVisible(true);
+			Load.setVisible(true);
+			Options.setVisible(true);
+			Back.setVisible(false);
+			
+			PencilLabel.setVisible(true);
+			GUImodeLabel.setVisible(false);
+			GUImode1Label.setVisible(false);
+			GUImode2Label.setVisible(false);
+			GUImode3Label.setVisible(false);
+			GUImode4Label.setVisible(false);
+			GUImode5Label.setVisible(false);
+			GUImode6Label.setVisible(false);
+			GUImode7Label.setVisible(false);
 		}//End of else if
 		
 		//Quit GUI + End code
@@ -293,10 +412,39 @@ public class Clicker_Game implements ActionListener
 		}//End of else if
 		
 		//Go back to top
-		else if (e.getActionCommand() == Actions.RECALCULATE.name())
+		else if (e.getActionCommand() == Actions.OPTIONS.name())
 		{//Beginning of else if
-			frame.dispose();
-			main(null);
+			options();
 		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE1.name())
+		{//Beginning of else if
+			frame.setSize(1280, 800);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE2.name())
+		{//Beginning of else if
+			frame.setSize(2048,1280);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE3.name())
+		{//Beginning of else if
+			frame.setSize(1650,1050);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE4.name())
+		{//Beginning of else if
+			frame.setSize(1440,900);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE5.name())
+		{//Beginning of else if
+			frame.setSize(1152,720);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE6.name())
+		{//Beginning of else if
+			frame.setSize(1024,768);
+		}//End of else if
+		else if (e.getActionCommand() == Actions.GUIMODE7.name())
+		{//Beginning of else if
+			frame.setSize(800,600);
+		}//End of else if
+		
+		
 	}//End of actionPerformed
 }//End of Clicker_Game
